4ae5ed8ed9cf6a5a994f7997f59c71e40472c068
jbs-17 <jatibangun17@gamil.com>
2025-12-07 18:11:19 +0700

    layouting the calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,41 +1,111 @@
 const globalClicks = [];
 
-window.addEventListener('click', async (e) => {
-  try {
-    for (const cb of globalClicks) {
-      await cb(e);
-    }
-  } catch (e) {
-    console.error(e);
-  }
-});
-
 {
-  const output = document.querySelector('section input[readonly]');
-  
-  globalClicks.push(async ({ target }) => {
-    if (target?.parentElement?.parentElement?.getAttribute('role') !== 'input') return;
-    
-    const { value } = target;
-    switch (value) {
-      case '=':
-        output.value = eval(
-          `${output.value||''}`.replaceAll('×', '*').replaceAll('÷', '/')
-          
-            
-        );
-        break;
-      case 'AC':
-        output.value = '';
-        break;
-      case 'x':
-        output.value = `${output.value||''}`.slice(0, -1);
-        break;
-      default:
-        output.value += value;
+  window.addEventListener('click', async (e) => {
+    try {
+      for (const cb of globalClicks) {
+        await cb(e);
+      }
+    } catch (e) {
+      console.error(e);
     }
-    
-    
-    
-  })
+  });
 }
+
+
+class Calculator {
+  constructor() {
+
+    this.events = {
+      label: {
+        'percent': false
+      }
+    }
+
+    this.in = {
+      btn0() {
+
+        document.querySelector('button[value="0"]').click();
+      },
+      btn00() {
+        for (let i = 0; i < 1; i++) this.btn0();
+      }
+    };
+
+    this.out = {
+      element: document.querySelector('[role="output"] input'),
+    }
+
+
+    this.render = Object.freeze({
+
+      rendere : ()=>{
+        this.render.rendere.apply.apply.apply.apply.apply.apply.apply.  .Calculator.Calculator.Calculator.Calculator. .Calculator.Calculator.Calculator.Calculator.Calculator.Calculator.Calculator.
+      }
+    });
+    
+    
+
+
+
+    globalClicks.push((e) => {
+      const { innerHTML: label } = e.target ?? {};
+      this.listen(label)
+    });
+  }
+
+  history = [];
+  container = [];
+
+
+
+
+
+
+
+  listen(label = '', value = '') {
+    this.out.element.focus();
+
+    value = label;
+    if (this.events.label.percent) {
+      return
+    };
+
+    if (label === '=')
+      return this.eval();
+
+    if (label === '00')
+      return this.in.btn00()
+
+    if (label === '%') {
+      this.events.label.percent = true;
+      value = '/100*'
+    }
+
+
+
+
+    this.container.push({ label, value });
+    this.render();
+  }
+
+
+
+  eval() {
+    let result = null;
+    try {
+      result = eval();
+    } catch (error) {
+      this.render.error(error);
+    };
+    this.container = [['result', result]];
+
+    this.history.push({
+      date: new Date(),
+      math: [...this.container],
+      result
+    });
+    this.render.result();
+  }
+}
+new Calculator();
